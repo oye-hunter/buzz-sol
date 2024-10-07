56be9de2b55dd82ebc1051a9d552d38d787c3410
Hunter <muhammadhassanmughal47@gmail.com>
2024-10-08 00:14:04 +0500

    made appointment calender form and made it more responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import "./NavBar.css";
+import React, { useState, useEffect } from 'react';
+import './NavBar.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, useLocation } from 'react-router-dom';
 
 function NavBar() {
   const location = useLocation(); // Get the current route
+  const [navbarBackground, setNavbarBackground] = useState(false);
+
+  // Update navbar background based on scroll position
+  const handleScroll = () => {
+    if (window.scrollY > 500) {
+      setNavbarBackground(true);
+    } else {
+      setNavbarBackground(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <Navbar expand="lg" className="the-navbar" style={{ padding: "20px 5vw", zIndex: "1000" }}>
+    <Navbar
+      expand="lg"
+      className={`the-navbar ${navbarBackground ? 'scrolled' : 'transparent'}`}
+      style={{ padding: '20px 5vw', zIndex: '1000' }}
+    >
       <Navbar.Brand href="/">
         <img
-          style={{ position: "relative", bottom: "2px" }}
-          src={require("../../assets/buzzsols-logo.png")}
+          style={{ position: 'relative', bottom: '2px' }}
+          src={require('../../assets/buzzsols-logo.png')}
           width="60"
           alt="Buzz Solutions logo"
         />{' '}
