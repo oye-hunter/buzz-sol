ea51e739d843f2ee328bc414c0fec99febe15cbb
Hunter <muhammadhassanmughal47@gmail.com>
2024-10-07 22:32:09 +0500

    made appointment calender form and made it more responsive

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './PricingCard.css'
 
-const Products = () => {
+const PricingCard = () => {
   // projectsData with satisfactionRate and no description or status
-  const projectsData = [
+  const pricingCardData = [
     {
       title: "Full Stack Development",
       projectsCompleted: 35,
@@ -36,12 +37,12 @@ const Products = () => {
   ];
 
   return (
-    <div className="products-container">
+    <div className="pricing-card-container">
       <h3 className="2xl:text-6xl xl:text-5xl lg:text-4xl md:text-3xl text-2xl font-semibold mb-3">
         Our <span style={{ color: "rgb(199,47,72)" }}>Services</span>
       </h3>
       <div className="products-card-container">
-        {projectsData.map((project, index) => (
+        {pricingCardData.map((project, index) => (
           <div key={index} className="products-card text-center sm:text-xl text-lg flex flex-col justify-center items-center">
             <h5>{project.title}</h5>
             <p className="mt-2 text-center font-light sm:text-sm text-xs">Projects Completed: {project.projectsCompleted}</p>
@@ -53,4 +54,4 @@ const Products = () => {
   );
 };
 
-export default Products;
+export default PricingCard;
