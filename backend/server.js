ed67f94f31270cf5aa75ce1ec5faf6478109e98b
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-10-10 00:34:15 +0500

    mongo connected

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,11 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Listen on port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
