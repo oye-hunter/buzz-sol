404ac3106679914c6969ae259e5d068328d6c36c
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-10-11 22:46:45 +0500

    backend setup

--- frontend/src/pages/BookAppointment/BookAppointment.jsx
+++ frontend/src/pages/BookAppointment/BookAppointment.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import axios from 'axios'; // Import Axios
 import emailjs from "@emailjs/browser"; // Import emailjs
 import CustomCalender from "../../components/CustomCalender/CustomCalender";
 import "./BookAppointment.css";
@@ -8,8 +9,6 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import InputAdornment from "@mui/material/InputAdornment";
 import Tooltip from "@mui/material/Tooltip";
 import Footer from '../../components/Footer/Footer';
-
-// Import FontAwesome icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
@@ -31,6 +30,8 @@ const BookAppointment = () => {
     email: "",
     phone: "",
     note: "",
+    serviceName: "", // Add serviceName for backend
+    amount: "" // Add amount for backend
   });
 
   // Ref for the form
@@ -47,9 +48,36 @@ const BookAppointment = () => {
     setSelectedTime(time);
   };
 
+  // Send data to the backend
+  const sendDataToBackend = async (appointmentData) => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/appointments', appointmentData);
+      if (response.status === 201) {
+        alert("Appointment successfully booked and saved to the database!");
+      } else {
+        alert("Failed to book the appointment. Please try again.");
+      }
+    } catch (error) {
+      console.error('Error saving appointment:', error);
+      alert("Failed to book the appointment. Please try again.");
+    }
+  };
+
   // Send email function using emailjs
   const sendEmail = (e) => {
     e.preventDefault();
+
+    // Appointment data for backend
+    const appointmentData = {
+      serviceName: formData.serviceName,
+      name: formData.fullName,
+      email: formData.email,
+      phoneNumber: formData.phone,
+      note: formData.note,
+      date: new Date(), // Assuming appointment is booked for the current date
+      time: selectedTime,
+      amount: formData.amount
+    };
 
     // Send email with the form data and selected time
     emailjs
@@ -63,6 +91,8 @@ const BookAppointment = () => {
         () => {
           console.log("SUCCESS!");
           alert("Appointment successfully booked!");
+          // Save the data to the backend after a successful email
+          sendDataToBackend(appointmentData);
         },
         (error) => {
           console.log("FAILED...", error.text);
@@ -82,114 +112,125 @@ const BookAppointment = () => {
 
   return (
     <>
-    <div className="book-apointment-container">
-      <h3 className="2xl:text-6xl xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-center font-semibold mt-5 mb-4">
-        Book An <span style={{ color: "rgb(199,47,72)" }}>Appointment</span>
-      </h3>
-      <form ref={formRef} onSubmit={sendEmail}>
-        <div className="flex items-center justify-center column-gap-5 row-gap-3 flex-col-reverse lg:flex-row">
-          <ThemeProvider theme={theme}>
-            <div className="flex flex-col row-gap-2">
-              <TextField
-                className="w-[350px]"
-                id="outlined-basic"
-                label="Full Name"
-                name="fullName"
-                variant="outlined"
-                value={formData.fullName}
-                onChange={handleInputChange}
-              />
-              <TextField
-                className="w-[350px]"
-                id="outlined-basic"
-                label="Email"
-                name="email"
-                variant="outlined"
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-              <TextField
-                className="w-[350px]"
-                id="outlined-basic"
-                label="Phone"
-                name="phone"
-                variant="outlined"
-                value={formData.phone}
-                onChange={handleInputChange}
-              />
+      <div className="book-apointment-container">
+        <h3 className="2xl:text-6xl xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-center font-semibold mt-5 mb-4">
+          Book An <span style={{ color: "rgb(199,47,72)" }}>Appointment</span>
+        </h3>
+        <form ref={formRef} onSubmit={sendEmail}>
+          <div className="flex items-center justify-center column-gap-5 row-gap-3 flex-col-reverse lg:flex-row">
+            <ThemeProvider theme={theme}>
+              <div className="flex flex-col row-gap-2">
+                <TextField
+                  className="w-[350px]"
+                  label="Full Name"
+                  name="fullName"
+                  variant="outlined"
+                  value={formData.fullName}
+                  onChange={handleInputChange}
+                />
+                <TextField
+                  className="w-[350px]"
+                  label="Email"
+                  name="email"
+                  variant="outlined"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+                <TextField
+                  className="w-[350px]"
+                  label="Phone"
+                  name="phone"
+                  variant="outlined"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                />
+                <TextField
+                  className="w-[350px]"
+                  label="Service Name"
+                  name="serviceName"
+                  variant="outlined"
+                  value={formData.serviceName}
+                  onChange={handleInputChange}
+                />
+                <TextField
+                  className="w-[350px]"
+                  label="Amount"
+                  name="amount"
+                  type="number"
+                  variant="outlined"
+                  value={formData.amount}
+                  onChange={handleInputChange}
+                />
+                <TextField
+                  className="w-[350px]"
+                  label="Note"
+                  name="note"
+                  variant="outlined"
+                  value={formData.note}
+                  onChange={handleInputChange}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <Tooltip
+                          title="Notes To Help Us Prepare For The Meeting?"
+                          arrow
+                        >
+                          <FontAwesomeIcon
+                            icon={faCircleInfo}
+                            style={{
+                              color: "rgb(199, 47, 72)",
+                              cursor: "pointer",
+                            }}
+                          />
+                        </Tooltip>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                {/* Hidden field to include selected time */}
+                <input
+                  type="hidden"
+                  name="selected_time"
+                  value={selectedTime || ""}
+                />
+              </div>
+            </ThemeProvider>
 
-              {/* Note field with Font Awesome Info icon on the right side */}
-              <TextField
-                className="w-[350px]"
-                id="outlined-note"
-                label="Note"
-                name="note"
-                variant="outlined"
-                value={formData.note}
-                onChange={handleInputChange}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Tooltip
-                        title="Notes To Help Us Prepare For The Meeting?"
-                        arrow
-                      >
-                        <FontAwesomeIcon
-                          icon={faCircleInfo}
-                          style={{
-                            color: "rgb(199, 47, 72)",
-                            cursor: "pointer",
-                          }}
-                        />
-                      </Tooltip>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              {/* Hidden field to include selected time */}
-              <input
-                type="hidden"
-                name="selected_time"
-                value={selectedTime || ""}
-              />
-            </div>
-          </ThemeProvider>
-
-          <div className="flex items-center justify-center flex-col md:flex-row row-gap-3">
-            <CustomCalender />
-            {/* Display the time buttons */}
-            <div className="time-buttons-container flex flex-col row-gap-2">
-              <h4 className="text-lg text-center font-semibold text-[#C72F48]">
-                Select Time
-              </h4>
-              {timeSlots.map((time, index) => (
-                <Button
-                  key={index}
-                  className={`time-button ${
-                    selectedTime === time ? "selected" : ""
-                  }`}
-                  variant="outline-primary"
-                  onClick={() => handleTimeClick(time)}
-                >
-                  {time}
-                </Button>
-              ))}
+            <div className="flex items-center justify-center flex-col md:flex-row row-gap-3">
+              <CustomCalender />
+              {/* Display the time buttons */}
+              <div className="time-buttons-container flex flex-col row-gap-2">
+                <h4 className="text-lg text-center font-semibold text-[#C72F48]">
+                  Select Time
+                </h4>
+                {timeSlots.map((time, index) => (
+                  <Button
+                    key={index}
+                    className={`time-button ${
+                      selectedTime === time ? "selected" : ""
+                    }`}
+                    variant="outline-primary"
+                    onClick={() => handleTimeClick(time)}
+                  >
+                    {time}
+                  </Button>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center mt-3">
-          <Button
-            className="bg-[#C72F48] hover:bg-[#a92339] border-[#C72F48] hover:border-[#a92339] w-[350px] text-2xl mb-2"
-            variant="primary"
-            type="submit"
-          >
-            Submit
-          </Button>
-        </div>
-      </form>
-    </div>
-      <Footer/>
-      </>
+          <div className="flex justify-center mt-3">
+            <Button
+              className="bg-[#C72F48] hover:bg-[#a92339] border-[#C72F48] hover:border-[#a92339] w-[350px] text-2xl mb-2"
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
+      <Footer />
+    </>
   );
 };
 
