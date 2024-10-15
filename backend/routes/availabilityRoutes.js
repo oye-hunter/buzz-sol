cc67dcc9ff5fd76598ccb8ca0d3df8dc216649e4
HozefaRauf <muhammadhozaifa24@gmail.com>
2024-10-16 01:37:53 +0500

    routes changed

--- backend/routes/availabilityRoutes.js
+++ backend/routes/availabilityRoutes.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const Appointment = require('../models/Appointment'); // Ensure you have the Appointment model imported
+const Appointment = require('../models/Appointment');
 const router = express.Router();
 
-// Route to get available time slots for a specific date
 router.get('/available-times', async (req, res) => {
-  const { date } = req.query; // Expect date in query parameters
+  const { date } = req.query;
 
   try {
     const bookedAppointments = await Appointment.find({ date: new Date(date) });
     const bookedTimes = bookedAppointments.map(app => app.time);
 
-    // Filter out booked times from the available time slots
     const allTimeSlots = ["10 PM (UTC+5)", "11 PM (UTC+5)", "12 AM (UTC+5)", "1 AM (UTC+5)", "2 AM (UTC+5)", "3 AM (UTC+5)"];
     const availableTimes = allTimeSlots.filter(time => !bookedTimes.includes(time));
 
