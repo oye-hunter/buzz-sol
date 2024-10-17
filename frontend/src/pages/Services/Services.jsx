751b20e9b337455060229c1970c955e8bea0394c
Usman Malik <usmanmalikk2004@gmail.com>
2024-10-17 20:29:50 +0500

    fixed services section added process section in it

--- frontend/src/pages/Services/Services.jsx
+++ frontend/src/pages/Services/Services.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./Services.css";
 import PricingCard from "../../sections/PricingSection/PricingSection";
 import Footer from '../../components/Footer/Footer';
+import ProcessSection from "../../sections/Process/ProcessSection";
 const Services = () => {
   return (<>
+  
     <div className="services-container">
         <PricingCard/>
     </div>
+    <ProcessSection/>
         <Footer/>
   </>
   );
